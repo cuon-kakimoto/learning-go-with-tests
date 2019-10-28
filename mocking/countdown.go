e8dacb652682e40c5c774df994d7c18dcee7a394
Shinji Kakimoto <skakimoto@cuon.co.jp>
2019-10-28 14:21:35 +0900

    add mocking

--- mocking/countdown.go
+++ mocking/countdown.go
@@ -11,16 +11,37 @@ import (
 const finalWord = "Go!"
 const countdownStart = 3
 
-func Countdown(out io.Writer){
-	for i := countdownStart; i >0; i--{
-		time.Sleep(1 * time.Second)
+type Sleeper interface {
+	Sleep()
+}
+// type DefaultSleeper struct{}
+
+type ConfigurableSleeper struct{
+	duration time.Duration
+  // HACK: structの中に関数も設定できるのね。
+	sleep func(time.Duration)
+}
+
+func (c *ConfigurableSleeper) Sleep(){
+	c.sleep(c.duration)
+}
+
+// Since we are using the ConfigurableSleeper, it is now safe to delete the DefaultSleeper implementation. Wrapping up our program and having a more generic Sleeper with arbitrary long countdowns.
+// func (d *DefaultSleeper) Sleep(){
+// 	time.Sleep(1 * time.Second)
+// }
+
+func Countdown(out io.Writer, sleeper Sleeper){
+	for i := countdownStart; i > 0; i--{
+		sleeper.Sleep()
 		fmt.Fprintln(out, i)
 	}
 
-	time.Sleep(1 * time.Second)
+	sleeper.Sleep()
 	fmt.Fprint(out, finalWord)
 }
 
 func main(){
-	Countdown(os.Stdout)
+	sleeper := &ConfigurableSleeper{1 * time.Second, time.Sleep}
+	Countdown(os.Stdout, sleeper)
 }
