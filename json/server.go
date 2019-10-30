7f2a9d65803a0300741641ed62610016acccccaf
Shinji Kakimoto <skakimoto@cuon.co.jp>
2019-10-30 10:40:58 +0900

    add embedding

--- json/server.go
+++ json/server.go
@@ -12,27 +12,28 @@ type PlayerStore interface {
 
 type PlayerServer struct {
 	// HACK: interfaceを型として保存できる。
-	store  PlayerStore
-	router *http.ServeMux
+	store PlayerStore
+	// HACK: この構造体内に、http.HandlerのIFを埋め込んでいるってことらしい。なので、http.Handlerの機能を持つようになる。
+	// router http.ServeMux and replaced it with http.Handler; this is called embedding.
+	// Go does not provide the typical, type-driven notion of subclassing, but it does have the ability to “borrow” pieces of an implementation by embedding types within a struct or interface.
+	http.Handler
 }
 
 // It's quite odd (and inefficient) to be setting up a router as a request comes in and then calling it.
 // リクエストのたびに、ルーティング設定をいちから作るのは不毛なので、事前に作るメソッドを用意する
 func NewPlayerServer(store PlayerStore) *PlayerServer {
-	p := &PlayerServer{
-		store,
-		http.NewServeMux(),
-	}
+	p := new(PlayerServer)
+	p.store = store
+
+	router := http.NewServeMux()
 	// HACK: routes機能があるのか。
 	// Go has a built-in routing mechanism called ServeMux (request multiplexer) which lets you attach http.Handlers to particular request paths.
-	p.router.Handle("/league", http.HandlerFunc(p.leagueHandler))
-	p.router.Handle("/players/", http.HandlerFunc(p.playersHandler))
+	router.Handle("/league", http.HandlerFunc(p.leagueHandler))
+	router.Handle("/players/", http.HandlerFunc(p.playersHandler))
+
+	p.Handler = router
 
 	return p
-}
-
-func (p *PlayerServer) ServeHTTP(w http.ResponseWriter, r *http.Request) {
-	p.router.ServeHTTP(w, r)
 }
 
 func (p *PlayerServer) leagueHandler(w http.ResponseWriter, r *http.Request) {
